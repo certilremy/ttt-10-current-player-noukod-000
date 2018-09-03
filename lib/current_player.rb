32fdf3cf1ec55e902496679ac36b7263ac0c5388
Certil Remy <certil@noukod.com>
2018-09-03 20:29:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
-def turn_count
-
-
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if (position == "X" || position == "O")
+      counter += 1
+    end
+  end
+  return counter
 end
 
 def current_player (board)
